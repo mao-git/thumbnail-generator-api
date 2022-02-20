284a88537828ebb790a4e62885b6d426cc321eea
u550426 <mlandaco@proveedor.teco.com.ar>
2022-02-20 18:41:16 -0300

    se agrega test unitario sobre la clase rest.

---
+++ src/test/java/com/example/rest/ImageUploadRestControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.rest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class ImageUploadRestControllerTest {
+
+	@Test
+	@Disabled
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
